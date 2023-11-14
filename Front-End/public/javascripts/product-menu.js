071e28291584e2ee3530c5c60a34b15631ffb03a
Roseedee Cehleah <roseedee2002@gmail.com>
2023-11-14 23:17:19 +0700

    create info in a product

--- Front-End/public/javascripts/product-menu.js
+++ Front-End/public/javascripts/product-menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".product-group .product-item .product-item-content").click(function(){
+    $(".product-group .product-group-item .product-group-item-content").click(function(){
       $(this).children('ul').slideToggle('500');
       // $(this).prev().css({'background-color':' yellow'});
     });
